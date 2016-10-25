7e4f5fd3493fcac8e242812b794f4540050789c8
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-10-26 02:08:14 +0900

    read json file and create stub.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 
+if (process.argv.length < 3) {
+  console.log("input json name.")
+  return;
+}
+const jsonFile = process.argv[2];
+
 const stream = fs.createWriteStream('./app.js', 'utf-8');
 
 const header = "" +
@@ -9,7 +15,17 @@ const header = "" +
 "app.use(bodyParser.json());\n\n";
 stream.write(header);
 
-// TODO
+const jsonString = fs.readFileSync(jsonFile, 'utf-8');
+const datas = JSON.parse(jsonString);
+datas.forEach((data) => {
+  let method = "";
+  method += "app." + data.method.toLowerCase() + "(\"" + data.path + "\", (req, res) => {\n";
+
+  // TODO: create stub
+
+  method += "});\n\n";
+  stream.write(method);
+});
 
 const footer = "app.listen(3000);\n";
 stream.write(footer);
